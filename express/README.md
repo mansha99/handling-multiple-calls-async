98ef8d3eb414d43880b7bc1c7c408af96d6fc498
Shivam Vishwakarma <svishwakarma74.com>
2020-02-28 14:30:38 +0530

    Initial Commit

---
+++ express/README.md
@@ -0,0 +1,30 @@
+## Mission:
+
+A client needs to know what is happening on the social networks. All of them. Right now.
+
+The three social networks the client is interested in are:
+
+https://takehome.io/twitter
+
+https://takehome.io/facebook    
+
+https://takehome.io/instagram
+
+
+Because these social networks are so webscale, they don't always respond predictably. The delay in their response almost appears like someone waited for a random integer of seconds before responding!
+
+Also, sometimes they will respond with an error. This error will not be valid JSON. Life's hard sometimes.
+
+The client needs to be able to run your thing, then issue the command:
+
+```
+curl localhost:3000
+```
+
+And get back a JSON response of the output from the three social networks in the format:
+
+```
+{ twitter: [tweets], facebook: [statuses], instagram: [photos] }
+```
+
+Order isn't important.
---
+++ kotlin/src/main/kotlin/com/opsly/backend/webfluxkotlin/MsWebClient.kt
@@ -0,0 +1,53 @@
+package com.mansha99.backend.webfluxkotlin
+
+import org.springframework.ui.set
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.http.HttpStatus
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+
+data class TwitterResponseObject(var username: String = "", var tweet: String = "")
+data class FacebookResponseObject(var name: String = "", var status: String = "")
+data class InstaResponseObject(var username: String = "", var picture: String = "")
+class MyCustomException() : Exception(){
+}
+class TwitterResponseObjectList : ArrayList<TwitterResponseObject>(){}
+class FacebookResponseObjectList : ArrayList<FacebookResponseObject>(){}
+class InstaResponseObjectList : ArrayList<InstaResponseObject>(){}
+
+@Service
+class MsWebClient {
+        
+        fun listTwitter():Mono<TwitterResponseObjectList>  {
+            val client = WebClient.create("https://takehome.io/")
+        return client.get()
+            .uri("twitter")
+            .retrieve()
+            .onStatus({ httpStatus -> HttpStatus.INTERNAL_SERVER_ERROR == httpStatus }, 
+            { 
+                clientResponse -> Mono.error(MyCustomException())
+            })
+            .bodyToMono(TwitterResponseObjectList::class.java);
+        }
+        fun listFacebook():Mono<FacebookResponseObjectList>  {
+            val client = WebClient.create("https://takehome.io/")
+        return client.get()
+            .uri("facebook")
+            .retrieve()
+            .onStatus({ httpStatus -> HttpStatus.INTERNAL_SERVER_ERROR == httpStatus }, 
+            { clientResponse -> Mono.error(MyCustomException()) })
+            .bodyToMono(FacebookResponseObjectList::class.java);
+        }
+        fun listInsta():Mono<InstaResponseObjectList>  {
+            val client = WebClient.create("https://takehome.io/")
+        return client.get()
+            .uri("instagram")
+            .retrieve()
+            .onStatus({ httpStatus -> HttpStatus.INTERNAL_SERVER_ERROR == httpStatus }, 
+            { clientResponse -> Mono.error(MyCustomException()) })
+            .bodyToMono(InstaResponseObjectList::class.java);
+        }
+        
+}
